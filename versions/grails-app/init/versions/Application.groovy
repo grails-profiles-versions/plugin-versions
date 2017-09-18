f402d2a6cd0fe28ab44c2a9f966958e68a2fc1a8
sdelamo <sergio.delamo@softamo.com>
2017-09-18 17:31:28 +0200

    3.1.0

--- versions/grails-app/init/versions/Application.groovy
+++ versions/grails-app/init/versions/Application.groovy
@@ -1,9 +1,9 @@
 package versions
 
-import grails.boot.*
+import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 
-class Application extends GrailsAutoConfiguration implements GrailsPluginApplication {
+class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
     }
