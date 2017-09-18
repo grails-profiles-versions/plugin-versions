a508efc00d4317d04c3716d736adef432165beb9
sdelamo <sergio.delamo@softamo.com>
2017-09-18 17:30:56 +0200

    3.0.17

--- versions/src/main/groovy/versions/VersionsGrailsPlugin.groovy
+++ versions/src/main/groovy/versions/VersionsGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import grails.plugins.*
 class VersionsGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.0.16 > *"
+    def grailsVersion = "3.0.17 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
